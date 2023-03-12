4bcd2445b7832a5971bd281def6adef04681f3ec
underway <underwayu@gmail.com>
2023-03-12 03:39:19 +0100

    Make Dependency convertible to/from JSON

--- stardust/stardust/dependency/dependency.h
+++ stardust/stardust/dependency/dependency.h
@@ -5,14 +5,17 @@
 #include <fstream>
 #include <functional>
 #include <optional>
+#include <chrono>
 
 #include <spdlog/spdlog.h>
 #include <fmt/format.h>
+#include <nlohmann/json.hpp>
 
 #include "../insertable.h"
 #include "../not_found_exception.h"
 
 namespace fs = std::filesystem;
+using json = nlohmann::json;
 
 namespace stardust {
 	class Dependency {
@@ -30,8 +33,24 @@ namespace stardust {
 			));
 		}
 
+		Dependency(const json& j) : dependent_path(j.at("path")), last_write_time(
+			std::chrono::time_point<fs::_File_time_clock>(
+				std::chrono::milliseconds(j["timestamp"].get<long>()))
+			) {}
+
+		json toJson() const {
+			auto j{ json({ "path", dependent_path.string() }) };
+			if (last_write_time.has_value()) {
+				j["timestamp"] = last_write_time.value().time_since_epoch().count();
+			}
+			else {
+				j["timestamp"] = nullptr;
+			}
+			return j;
+		}
+
 		bool operator==(const Dependency& other) const {
-			return dependent_path == other.dependent_path;
+			return dependent_path == other.dependent_path && last_write_time == other.last_write_time;
 		}
 	};
 }
