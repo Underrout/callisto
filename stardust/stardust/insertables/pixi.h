2c6548d3f0f0066b544806bca93e54c6ec93653b
underway <underwayu@gmail.com>
2023-03-14 11:00:55 +0100

    Remove extra byte stuff (for now)
    Probably best to just assume MeiMei may need to run if any PIXI related file changes, also simpler.

--- stardust/stardust/insertables/pixi.h
+++ stardust/stardust/insertables/pixi.h
@@ -15,22 +15,11 @@ namespace fs = std::filesystem;
 
 namespace stardust {
 	class Pixi : public RomInsertable {
-	public:
-		struct ExtraByteInfo {
-			fs::path config_file_path;
-			int byte_count;
-			int extra_byte_count;
-
-			ExtraByteInfo(const fs::path& config_file_path, int byte_count, int extra_byte_count)
-				: config_file_path(config_file_path), byte_count(byte_count), extra_byte_count(extra_byte_count) {}
-		};
-
 	protected:
 		const fs::path pixi_folder_path;
 		const std::string pixi_options;
 		const std::vector<fs::path> static_dependencies;
 		const std::optional<fs::path> dependency_report_file_path;
-		std::vector<ExtraByteInfo> extra_bytes;
 		
 		std::unordered_set<Dependency> determineDependencies() override;
 
@@ -40,7 +29,5 @@ namespace stardust {
 			const std::optional<fs::path>& dependency_report_file_path = {});
 
 		void insert() override;
-
-		const std::vector<ExtraByteInfo>& getExtraByteInfo() const;
 	};
 }
