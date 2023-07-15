d90b5b74fb890a31f9ecbe5df18d05ff7a3f3ed5
underway <underwayu@gmail.com>
2023-07-16 02:23:05 +0300

    Ensure level filenames conform to format prior to build
    Turns out with Lunar Helper (and callisto, by extension), it was possible to place a MWL like `level 7.mwl` in the levels folder, then build and have that level inside the ROM, but have it be exported by Lunar Monitor/Helper as `level 007.mwl` later on, which would cause there to be two distinct levels both being inserted as level 7, one after the other, the latter overwriting the earlier one.
    
     I'm now working around this by ensuring all files in the levels folder follow the `level ###.mwl` format prior to builds. The way I do this is:
    
    - if the filename doesn't conform to the format, I ask the user if they want it to be renamed to `level X.mwl` where `X` is the level number the MWL points to (I do have to lightly inspect the MWL's contents for this)
    
    - if the filename *does* conform to the format, but the level number in the filename doesn't match the one in the MWL, I ask the user if they would like to either rename the file or point the file to a different level
    
    The first is likely to happen when people drop levels straight into the levels folder from external sources. The latter occurs when people think they can just create a copy of the MWL and rename it, neglecting to consider that the filename actually has nothing to do with the level number from Lunar Magic's perspective.
    
    Note that I currently take user input on these prompts with enter, which has the unfortunate effect of now throwing the user out of the command line view immediately when the build process finishes. I hate all this "press enter"/"press ESC" shit and wish to be done with it, please and thank you. Maybe I'll just scrap FTXUI and go back to a plaintext menu, this shit's lowkey real annoying.

--- callisto/insertables/levels.cpp
+++ callisto/insertables/levels.cpp
@@ -3,7 +3,8 @@
 namespace callisto {
 	Levels::Levels(const Configuration& config)
 		: LunarMagicInsertable(config), 
-		levels_folder(registerConfigurationDependency(config.levels, Policy::REINSERT).getOrThrow())
+		levels_folder(registerConfigurationDependency(config.levels, Policy::REINSERT).getOrThrow()),
+		allow_user_input(config.allow_user_input)
 	{
 		registerConfigurationDependency(config.level_import_flag);
 
@@ -20,6 +21,170 @@ namespace callisto {
 				levels_folder.string()
 			));
 		}
+	}
+
+	void Levels::normalizeMwls() {
+		std::vector<fs::path> mwl_paths{};
+		for (const auto& entry : fs::directory_iterator(levels_folder)) {
+			if (entry.path().extension() == ".mwl") {
+				mwl_paths.push_back(entry);
+			}
+		}
+
+		for (const auto& entry : mwl_paths) {
+			const auto mwl_level_number{ getInternalLevelNumber(entry) };
+			if (!mwl_level_number.has_value()) {
+				throw InsertionException(fmt::format(
+					"Could not determine source level number of level file '{}', this level file may be malformed",
+					entry.string()
+				));
+			}
+
+			const auto file_level_number{ getExternalLevelNumber(entry) };
+			if (!file_level_number.has_value()) {
+				if (allow_user_input) {
+					char answer;
+					spdlog::warn(
+						"Level file '{}' (pointing to level {:X}) has malformed filename, rename to 'level {:03X}.mwl'? y/n",
+						entry.string(), mwl_level_number.value(), mwl_level_number.value()
+					);
+					std::cin >> answer;
+					if (answer == 'y' || answer == 'Y') {
+						const auto target_path{ entry.parent_path() / fmt::format("level {:03X}.mwl", mwl_level_number.value()) };
+						try {
+							fs::rename(entry, target_path);
+						}
+						catch (const std::exception& e) {
+							throw InsertionException(fmt::format(
+								"Failed to rename level file '{}' to '{}' with exception:\n{}",
+								entry.string(), target_path.string(),
+								e.what()
+							));
+						}
+						continue;
+					}
+				}
+				throw InsertionException(fmt::format(
+					"Level file '{}' (pointing to level {:X}) has malformed filename",
+					entry.string(), mwl_level_number.value()
+				));
+			}
+
+			if (file_level_number != mwl_level_number) {
+				if (allow_user_input) {
+					char answer;
+					spdlog::warn(
+						"Level file '{}' points to level {:X} but filename says it should point to level {:X}, what should be done?\n\r"
+						"(a) Keep filename '{}', but point it at level {:X}\n\r"
+						"(b) Rename file to 'level {:03X}.mwl', keep pointing at level {:X}\n\r"
+						"(c) Abort",
+						entry.string(), mwl_level_number.value(), file_level_number.value(),
+						entry.filename().string(), file_level_number.value(),
+						mwl_level_number.value(), mwl_level_number.value()
+					);
+					std::cin >> answer;
+					if (answer == 'a' || answer == 'A') {
+						try {
+							setInternalLevelNumber(entry, file_level_number.value());
+						}
+						catch (const std::exception& e) {
+							throw InsertionException(fmt::format(
+								"Failed to set source level number of level file '{}' to {:X} with exception:\n{}",
+								entry.string(), file_level_number.value(),
+								e.what()
+							));
+						}
+						continue;
+					}
+					else if (answer == 'b' || answer == 'B') {
+						const auto target_path{ entry.parent_path() / fmt::format("level {:03X}.mwl", mwl_level_number.value()) };
+						try {
+							fs::rename(entry, target_path);
+						}
+						catch (const std::exception& e) {
+							throw InsertionException(fmt::format(
+								"Failed to rename level file '{}' to '{}' with exception:\n{}",
+								entry.string(), target_path.string(),
+								e.what()
+							));
+						}
+						continue;
+					}
+				}
+				throw InsertionException(fmt::format(
+					"Level file '{}' points to level {:X} but filename says it should point to level {:X}",
+					entry.string(), mwl_level_number.value(), file_level_number.value()
+				));
+			}
+		}
+	}
+
+	size_t Levels::readBytesAt(std::fstream& stream, size_t offset, size_t count) {
+		size_t out{ 0 };
+
+		stream.seekg(offset);
+		for (auto i{ 0 }; i != count; ++i) {
+			char c;
+			stream.read(&c, 1);
+			out |= static_cast<unsigned char>(c) << (8 * i);
+		}
+
+		return out;
+	}
+
+	std::optional<int> Levels::getInternalLevelNumber(const fs::path& mwl_path){
+		try {
+			std::fstream mwl_file(mwl_path, std::ios::binary | std::ios::in);
+
+			const auto data_pointer_table_pointer{ readBytesAt(mwl_file, MWL_DATA_POINTER_TABLE_POINTER_OFFSET, 4) };
+			const auto level_information_pointer{ readBytesAt(mwl_file, data_pointer_table_pointer, 4) };
+			const auto level_number{ readBytesAt(mwl_file, level_information_pointer, 2) };
+
+			if (level_number < 0x200) {
+				return level_number;
+			}
+			else {
+				return {};
+			}
+		}
+		catch (...) {
+			return {};
+		}
+	}
+
+	std::optional<int> Levels::getExternalLevelNumber(const fs::path& mwl_path) {
+		const auto mwl_name{ mwl_path.stem().string() };
+
+		if (!mwl_name.starts_with("level ")) {
+			return {};
+		}
+
+		if (!std::all_of(mwl_name.begin() + 6, mwl_name.end(), [](const auto& e) { return std::isalnum(e); })) {
+			return {};
+		}
+
+		try {
+			const auto level_number{ std::stoi(mwl_name.substr(6, 3), nullptr, 16) };
+			if (level_number < 0x200) {
+				return level_number;
+			}
+			return {};
+		}
+		catch (...) {
+			// we failed to parse the hexadecimal level number in some way
+			return {};
+		}
+	}
+
+	void Levels::setInternalLevelNumber(const fs::path& mwl_path, int level_number) {
+		std::fstream mwl_file(mwl_path, std::ios::binary | std::ios::in | std::ios::out);
+
+		const auto data_pointer_table_pointer{ readBytesAt(mwl_file, MWL_DATA_POINTER_TABLE_POINTER_OFFSET, 4) };
+		const auto level_information_pointer{ readBytesAt(mwl_file, data_pointer_table_pointer, 4) };
+		
+		mwl_file.seekg(level_information_pointer);
+		const char bytes[2] = { level_number & 0xFF, level_number >> 8 };
+		mwl_file.write(bytes, 2);
 	}
 
 	std::unordered_set<ResourceDependency> Levels::determineDependencies() {
@@ -41,6 +206,8 @@ namespace callisto {
 			spdlog::info("No levels to insert, skipping level insertion");
 			return;
 		}
+
+		normalizeMwls();
 
 		int exit_code;
 
--- callisto/insertables/levels.h
+++ callisto/insertables/levels.h
@@ -2,6 +2,7 @@
 
 #include <filesystem>
 #include <optional>
+#include <functional>
 
 #include <fmt/core.h>
 
@@ -19,8 +20,20 @@ namespace fs = std::filesystem;
 namespace callisto {
 	class Levels : public LunarMagicInsertable {
 	protected:
+		static constexpr auto MWL_DATA_POINTER_TABLE_POINTER_OFFSET{ 0x4 };
+
 		const fs::path levels_folder;
 		std::optional<std::string> import_flag;
+		const bool allow_user_input;
+
+		void normalizeMwls();
+
+		size_t readBytesAt(std::fstream& stream, size_t offset, size_t count);
+
+		std::optional<int> getInternalLevelNumber(const fs::path& mwl_path);
+		std::optional<int> getExternalLevelNumber(const fs::path& mwl_path);
+		void setInternalLevelNumber(const fs::path& mwl_path, int level_number);
+		void normalizeMwl(const fs::path& mwl_path);
 
 		std::unordered_set<ResourceDependency> determineDependencies() override;
 
